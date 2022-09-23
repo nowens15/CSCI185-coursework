65ed198a48299a4b3eec4d236fc7f1e796cce621
Nadia <hello@nadiaowens.com>
2022-09-23 16:23:13 -0400

    Tutorial 6

---
+++ Lectures/activity 5/01-style-property-demo/main.js
@@ -0,0 +1,28 @@
+
+const makeRed = () => {
+    // your code here...
+    console.log('Change background to red');
+    document.querySelector('body').style.backgroundColor = 'red';
+};
+
+const makeRainbow = () => {
+    // your code here...
+    console.log('Change background to red');
+    document.querySelector('body').style.backgroundColor = 'red';
+};
+
+const makeBlue = () => {
+    // your code here...
+    console.log('Change background to blue');
+};
+
+const makePink = () => {
+    // your code here...
+    console.log('Change background to pink');
+};
+
+const makeOrange = () => {
+    // your code here...
+    console.log('Change background to orange');
+};
+
