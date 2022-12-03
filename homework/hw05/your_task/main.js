1c7f3420f736cd9dcf3d5988318a63e6678322dd
Nadia <hello@nadiaowens.com>
2022-12-02 20:01:06 -0500

    homework5 and project 2

--- homework/hw05/your_task/main.js
+++ homework/hw05/your_task/main.js
@@ -2,8 +2,9 @@ const baseURL = 'https://www.apitutor.org/spotify/simple/v1/search';
 
 function search(ev) {
     const term = document.querySelector('#search').value;
-    console.log('search for:', term);
+    // console.log('search for:', term);
     // issue three Spotify queries at once...
+
     getTracks(term);
     getAlbums(term);
     getArtist(term);
@@ -17,19 +18,64 @@ async function getTracks(term) {
     console.log(tracksEndpoint);
     
     //asking to pull data to spotify 
+
     const data = await fetch(tracksEndpoint).then(response => response.json());
     console.log(data);
+    console.log(data[0]);
+    console.log(data[0].name);
+    console.log(data[0].artist.name);
+    console.log(data[0].album.image_url);
+
+    //clears out old search before running a new one
+
+    document.querySelector('#tracks').innerHTML = "";
+
+    for (let i=0; i<5; i++) {
+        const template = `
+        <section class="track-item preview" onclick="playSong('${data[i].id}')">
+                            <img src="${data[i].album.image_url}">
+                            <i class="fas play-track fa-play" aria-hidden="true"></i>
+                            <div class="label">
+                                <h2>${data[i].name}</h2>
+                                <p>
+                                ${data[i].artist.name}
+                                </p>
+                            </div>
+                        </section>
+        `;
+        document.querySelector('#tracks').insertAdjacentHTML('beforeend', template);
+    }
+}
+
+//job of playSong function, creates an iframe & replaces artist panel content
+
+function playSong(id){
+    const template = `
+    <iframe style="border-radius:12px" 
+        src="https://open.spotify.com/embed/track/${id}?utm_source=generator&theme=0" 
+        width="100%" 
+        height="352" 
+        frameBorder="0" 
+        allowfullscreen="" 
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+        loading="lazy"></iframe>
+    `;
+    document.querySelector('#artist').innerHTML = template;
 }
 
 async function getAlbums(term) {
     const albumsEndpoint = baseURL + "?q=" + term + "&type=album";
-    console.log(albumsEndpoint);
+
+    // console.log(albumsEndpoint);
     //asking to pull data to spotify 
+
     const data = await fetch(albumsEndpoint).then(response => response.json());
-    console.log(data);
+
+    // console.log(data);
 }
 
 async function getArtist(term) {
+
     /*
     The Plan:
     0. Create the URL Endpoint based on the term.
@@ -38,22 +84,28 @@ async function getArtist(term) {
     3. After data response, print to console.
     4. Figure out how to display artist tastefully.
     */
+
     const artistEndpoint = baseURL + "?q=" + term + "&type=artist";
-    console.log(artistEndpoint);
+
+    // console.log(artistEndpoint);
 
     //asking to pull data to spotify 
+
     const data = await fetch(artistEndpoint).then(response => response.json());
 
     //if else there is at least 1 element
+
     if (data.length === 0) {
         document.querySelector('#artist').innerHTML = "No result found.";
         return;
     }
-    console.log(data);
-    console.log(data[0].name);
-    console.log(data[0].img_url);
+
+    // console.log(data);
+    // console.log(data[0].name);
+    // console.log(data[0].img_url);
 
     //style and target artist results
+
     const template = `
    <section>
     <img src="${data[0].image_url}" />
@@ -87,8 +139,10 @@ async function getArtist(term) {
 
 
 document.querySelector('#search').onkeyup = function (ev) {
+
     // Number 13 is the "Enter" key on the keyboard
-    console.log(ev.keyCode);
+    // console.log(ev.keyCode);
+
     if (ev.keyCode === 13) {
         ev.preventDefault();
         search();
